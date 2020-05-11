4810dc3a703bb2fd15e6fa5bd776dfac5ec1b0e5
Lduffley <53620669+Lduffley@users.noreply.github.com>
2020-05-11 14:38:45 -0500

    Add files via upload

---
+++ lib/screens/loading_screen.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+
+class LoadingScreen extends StatefulWidget {
+  @override
+  _LoadingScreenState createState() => _LoadingScreenState();
+}
+
+class _LoadingScreenState extends State<LoadingScreen> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    getUserLocation();
+  }
+
+  void getUserLocation() async {
+    Position position = await Geolocator()
+        .getCurrentPosition(desiredAccuracy: LocationAccuracy.low);
+    print(position);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: RaisedButton(
+          onPressed: () {
+            getUserLocation();
+          },
+          child: Text('Get Location'),
+        ),
+      ),
+    );
+  }
+}
